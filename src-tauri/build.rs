fab9f4503fd1ba2b69f5900d73e8da1d4fd85335
Haillord <roga392@gmail.com>
2026-04-08 21:49:59 +0300

    312

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,3 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        println!("cargo:rustc-link-arg-bins=/MANIFEST:EMBED");
-        println!("cargo:rustc-link-arg-bins=/MANIFESTINPUT:ffstudio.exe.manifest");
-    }
+    tauri_build::build();
 }
